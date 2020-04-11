5534031c452fd90118e0a39532052ed92bd3482a
tayyab bhatti <tayyab.bhatti30@hotmail.com>
2020-04-11 23:01:25 +0500

    First Commit

---
+++ UploadingFileAtServer_Core/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using UploadingFileAtServer_Core.Models;
+
+namespace UploadingFileAtServer_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHostEnvironment _environment;
+        private readonly string _dir;
+        public HomeController(ILogger<HomeController> logger, IHostEnvironment environment)
+        {
+            _logger = logger;
+            this._environment = environment;
+            _dir = _environment.ContentRootPath;
+        }
+
+        public IActionResult UploadFile_JaveScript()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public  IActionResult SingleFileUpoad(IFormFile file)
+        {
+      
+            using(var fileStreem=new FileStream(Path.Combine(_dir,file.FileName),FileMode.Create, FileAccess.Write))
+            {
+
+                file.CopyTo(fileStreem);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult MultiPleFilesUpload(IEnumerable<IFormFile> files)
+        {
+            int i = 0;
+            foreach (var item in files)
+            {
+                using (var fileStreem = new FileStream(Path.Combine(_dir,$"Files_{i}.{item.ContentType}"), FileMode.Create, FileAccess.Write))
+                {
+                    item.CopyTo(fileStreem);
+                }
+         
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UploadingFileAtServer_Core/Controllers/VideoController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Enums;
+
+namespace UploadingFileAtServer_Core.Controllers
+{
+    public class VideoController : Controller
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string dir;
+
+
+        // if u are working on saving the Videos you have to installed the xabe.ffmpeg library from the  nuget pakage.. 
+
+
+
+        public VideoController(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+           dir = environment.WebRootPath;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(IFormFile file, double start, double end)
+        //{
+
+        //    using (var fileStreem = new FileStream(Path.Combine(dir, "file.mp4"), FileMode.Create, FileAccess.Write))
+        //    {
+
+        //        await file.CopyToAsync(fileStreem);
+
+
+
+        //        var input = Path.Combine(dir, "file.mp4");
+
+        //        var output = Path.Combine(dir, "converted.mp4");
+
+
+        //        FFmpeg.ExecutablesPath = Path.Combine(dir, "ffmpeg");
+
+
+        //        var startSpen = TimeSpan.FromSeconds(start);
+        //        var endSpan = TimeSpan.FromSeconds(end);
+
+        //        var duration = endSpan - startSpen;
+
+
+
+        //        var info = await MediaInfo.Get(input);
+
+        //        var videoStream = info.VideoStreams.First()
+        //            .SetCodec(VideoCodec.H264).SetSize(VideoSize.Hd480)
+        //            .Split(startSpen,endSpan);   // Splict fucntion is use to Split the Vide with Specific timeSpan
+
+        //        await Conversion.New()
+        //            .AddStream(videoStream).SetOutput(output).Start();
+
+        //    }
+
+        //    //     await ConvertVideo();
+        //    return RedirectToAction("Index");
+        //}
+
+
+
+
+        [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit =409715200)]
+        [RequestSizeLimit(409715200)]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+
+            using (var fileStreem = new FileStream(Path.Combine(dir, "file.mp4"), FileMode.Create, FileAccess.Write))
+            {
+
+                await file.CopyToAsync(fileStreem);
+
+                var input = Path.Combine(dir, "file.mp4");
+
+                var output = Path.Combine(dir, "converted.mp4");
+
+
+                FFmpeg.ExecutablesPath = Path.Combine(dir, "ffmpeg");
+
+
+                var info = await MediaInfo.Get(input);
+
+                var videoStream = info.VideoStreams.First()
+                    .SetCodec(VideoCodec.H264).SetSize(VideoSize.Hd480);
+
+                await Conversion.New()
+                    .AddStream(videoStream).SetOutput(output).Start();
+
+            }
+
+            //     await ConvertVideo();
+            return RedirectToAction("Index");
+        }
+
+
+
+        public async Task<bool> ConvertVideo()
+        {
+
+            try
+            {
+          
+                var input = Path.Combine(dir, "file.mp4");
+
+                var output = Path.Combine(dir, "converted.mp4");
+
+
+                FFmpeg.ExecutablesPath = Path.Combine(dir, "ffmpeg");
+
+
+                var info = await MediaInfo.Get(input);
+
+                var videoStream = info.VideoStreams.First()
+                    .SetCodec(VideoCodec.H264).SetSize(VideoSize.Hd480);
+
+                await Conversion.New()
+                    .AddStream(videoStream).SetOutput(output).Start();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            return true;
+        }
+    }
+}
